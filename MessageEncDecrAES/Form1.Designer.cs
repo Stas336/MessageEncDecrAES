1390647914ba7fbe9a17d21ccc3ae0c97ea89c12
Stas Loyko <stas336@gmail.com>
2017-02-20 01:02:03 +0300

    Upload on Git

---
+++ MessageEncDecrAES/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace MessageEncDecrAES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.origText = new System.Windows.Forms.TextBox();
+            this.key = new System.Windows.Forms.TextBox();
+            this.encrButton = new System.Windows.Forms.Button();
+            this.modText = new System.Windows.Forms.TextBox();
+            this.decrButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // origText
+            // 
+            this.origText.Location = new System.Drawing.Point(12, 12);
+            this.origText.Multiline = true;
+            this.origText.Name = "origText";
+            this.origText.Size = new System.Drawing.Size(433, 122);
+            this.origText.TabIndex = 0;
+            // 
+            // key
+            // 
+            this.key.Location = new System.Drawing.Point(12, 268);
+            this.key.Name = "key";
+            this.key.Size = new System.Drawing.Size(433, 20);
+            this.key.TabIndex = 1;
+            // 
+            // encrButton
+            // 
+            this.encrButton.Location = new System.Drawing.Point(12, 294);
+            this.encrButton.Name = "encrButton";
+            this.encrButton.Size = new System.Drawing.Size(213, 52);
+            this.encrButton.TabIndex = 3;
+            this.encrButton.Text = "Encrypt";
+            this.encrButton.UseVisualStyleBackColor = true;
+            this.encrButton.Click += new System.EventHandler(this.encrButton_Click);
+            // 
+            // modText
+            // 
+            this.modText.Location = new System.Drawing.Point(12, 140);
+            this.modText.Multiline = true;
+            this.modText.Name = "modText";
+            this.modText.Size = new System.Drawing.Size(433, 122);
+            this.modText.TabIndex = 4;
+            // 
+            // decrButton
+            // 
+            this.decrButton.Location = new System.Drawing.Point(232, 294);
+            this.decrButton.Name = "decrButton";
+            this.decrButton.Size = new System.Drawing.Size(213, 52);
+            this.decrButton.TabIndex = 5;
+            this.decrButton.Text = "Decrypt";
+            this.decrButton.UseVisualStyleBackColor = true;
+            this.decrButton.Click += new System.EventHandler(this.decrButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 358);
+            this.Controls.Add(this.decrButton);
+            this.Controls.Add(this.modText);
+            this.Controls.Add(this.encrButton);
+            this.Controls.Add(this.key);
+            this.Controls.Add(this.origText);
+            this.Name = "Form1";
+            this.Text = "AES Message Encryption/Decryption Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox origText;
+        private System.Windows.Forms.TextBox key;
+        private System.Windows.Forms.Button encrButton;
+        private System.Windows.Forms.TextBox modText;
+        private System.Windows.Forms.Button decrButton;
+    }
+}
+
---
+++ MessageEncDecrAES/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MessageEncDecrAES
+{
+    public partial class Form1 : Form
+    {
+        private static RijndaelManaged aes;
+        private static int HASH_ITER = 10000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ToolTip toolTip = new ToolTip();
+            toolTip.SetToolTip(origText, "Enter here original text or encrypted text");
+            toolTip.SetToolTip(modText, "Result will be displayed here");
+            toolTip.SetToolTip(key, "Enter encryption/decryption key here");
+            aes = new RijndaelManaged();
+            aes.KeySize = 256;
+            aes.BlockSize = 128;
+            aes.Mode = CipherMode.CBC;
+            aes.Padding = PaddingMode.PKCS7;
+        }
+
+        private void encrButton_Click(object sender, EventArgs e)
+        {
+            if (key.Text.Length == 0 || origText.Text.Length == 0)
+            {
+                MessageBox.Show("Message or key is empty. Fill it before proceeding.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Rfc2898DeriveBytes aesKey = new Rfc2898DeriveBytes(key.Text, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, HASH_ITER);
+            aes.Key = aesKey.GetBytes(32);
+            aes.IV = aesKey.GetBytes(16);
+            byte[] bytesBuff = Encoding.Unicode.GetBytes(origText.Text);
+            using (MemoryStream mStream = new MemoryStream())
+            {
+                using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                    cStream.Close();
+                }
+                modText.Text = Convert.ToBase64String(mStream.ToArray());
+            }
+            aes.Clear();
+        }
+
+        private void decrButton_Click(object sender, EventArgs e)
+        {
+            if (key.Text.Length == 0 || origText.Text.Length == 0)
+            {
+                MessageBox.Show("Encrypted message or key is empty. Fill it before proceeding.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            Rfc2898DeriveBytes aesKey = new Rfc2898DeriveBytes(key.Text, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 }, HASH_ITER);
+            aes.Key = aesKey.GetBytes(32);
+            aes.IV = aesKey.GetBytes(16);
+            byte[] bytesBuff = Convert.FromBase64String(origText.Text);
+            try
+            {
+                using (MemoryStream mStream = new MemoryStream())
+                {
+                    using (CryptoStream cStream = new CryptoStream(mStream, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cStream.Write(bytesBuff, 0, bytesBuff.Length);
+                        cStream.Close();
+                    }
+                    modText.Text = Encoding.Unicode.GetString(mStream.ToArray());
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Wrong decryption key.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                aes.Clear();
+            }
+        }
+    }
+}
